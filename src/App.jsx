161fb1fd3aca7bf4864134ff3ab6c70fdb8d9c76
Fishniki <nikichum96@gmail.com>
2024-08-12 21:27:49 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './page/Home';
+
+function App() {
+  return (
+    <div className='overflow-hidden'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
