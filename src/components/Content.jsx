6f1745548818c949bb0bd0f811e1990ed9f694a2
irfancipinang <muhammadirfanm.2006@gmail.com>
2024-08-15 13:18:35 +0700

    new

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,12 @@
 import { FaArrowRight } from "react-icons/fa6";
 import Fisik from "../assets/img/sfisik2.png";
+import { MdAdd } from "react-icons/md";
+import { useState } from "react";
 
 const Content = () => {
+
+  const[page, setPage] = useState(true)
+
 return (
     <div className="mt-20 ">
         <div className="container px-6 mb-10 font-inter max-w-md mx-auto sm:max-w-xl md:max-w-5xl lg:max-w-full lg:px-0">
@@ -42,75 +47,88 @@ return (
         </div>
 
         <div className="p-6 w-full flex justify-center">
-  {/* Kontainer untuk kedua halaman */}
-  <div id="pembahasan" className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-6">
-    
-    {/* page1 */}
-    <div className="p-4">
-      <h2 className="mt-6 text-2xl font-semibold mb-3 text-slate-800 sm:text-3xl font-signika sm:mt-8 lg:mt-0">
-        Empat pillar <span className="text-sky-500">gizi</span> seimbang
-      </h2>
-      <div>
-        <ul className="max-w-[500px] space-y-2 font-roboto text-[17px] md:text-xl list-disc">
-          <li>Mengonsumsi anekaragam pangan dengan proporsi makanan yang seimbang 
-            (karbohidrat, lemak, protein, mineral dan vitamin)</li>
-          <li>Membiasakan perilaku hidup bersih</li>
-          <li>Melakukan aktivitas fisik yang teratur</li>
-          <li>Memantau Berat Badan (BB) secara teratur untuk mempertahankan berat badan normal</li>
-        </ul>
-      </div>
-    </div>
+        {/* Kontainer untuk kedua halaman */}
+        <div id="pembahasan" className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-6">
+          
+          {/* page1 */}
+          <div className="p-4">
+            <h2 className="mt-6 text-2xl font-semibold mb-3 text-slate-800 sm:text-3xl font-signika sm:mt-8 lg:mt-0">
+              Empat pillar <span className="text-sky-500">gizi</span> seimbang
+            </h2>
+            <div>
+              <ul className="max-w-[500px] space-y-2 font-roboto text-[17px] md:text-xl list-disc">
+                <li>Mengonsumsi anekaragam pangan dengan proporsi makanan yang seimbang 
+                  (karbohidrat, lemak, protein, mineral dan vitamin)</li>
+                <li>Membiasakan perilaku hidup bersih</li>
+                <li>Melakukan aktivitas fisik yang teratur</li>
+                <li>Memantau Berat Badan (BB) secara teratur untuk mempertahankan berat badan normal</li>
+              </ul>
+            </div>
+          </div>
 
-    {/* page2 */}
-    <div className="p-4">
-      <h2 className="mt-6 text-2xl mb-5 font-semibold text-slate-800 sm:text-3xl font-signika sm:mt-8 lg:mt-0">
-        Tips memilih makanan yang <span className="text-sky-500">bergizi</span>
-      </h2>
-      <div className="h-96 overflow-y-auto">
-        {[
-          {
-            title: 'Sistem tubuh berfungsi dengan baik',
-            items: [
-              'Pilih makanan yang seimbang sesuai dengan kebutuhan seperti Nutrisi dan Gizinya',
-              'Tubuh harus harus mendapatkan makanan yang seimbang seperti karbohidrat, protein, serat dan buah - buahan yang mengandung bnayaknya vitamin'
-            ]
-          },
-          {
-            title: 'Sistem tubuh berfungsi dengan baik',
-            items: [
-              'Sajikan makanan dalam bentuk yang sesuai dengan kebutuhan tubuh',
-              'Siapkan makanan sesuai dengan kapasitas makanan yang baik dan menyehatkan'
-            ]
-          },
-          {
-            title: 'Sistem tubuh berfungsi dengan baik',
-            items: [
-              'Makanlah makanan yang tidak mengandung perasa, pewarna dan pengawet buatan',
-              'Makanan yang mengandung gula, garam dan lemak yang berlebihan dianjurkan untuk dikurangin'
-            ]
-          },
-        ].map((section, index) => (
-          <div key={index} className="inline-block mb-1">
-            <div className="flex py-5 gap-2 px-5 rounded-lg shadow-lg border">
-              <div className="w-2 rounded-md bg-sky-500"></div>
-              <div>
-                <h1 className="font-medium font-signika">{section.title}</h1>
-                <ul className="list-disc pl-5 space-y-1">
-                  {section.items.map((item, idx) => (
-                    <li key={idx}>{item}</li>
-                  ))}
-                </ul>
+          {/* page2 */}
+          <div className="p-4">
+            <h2 className="mt-6 text-2xl mb-5 font-semibold text-slate-800 sm:text-3xl font-signika sm:mt-8 lg:mt-0">
+              Tips hidup sehat <span className="text-sky-500">bergizi</span>
+            </h2>
+            <div className="h-96 overflow-y-auto">
+              {[
+                {
+                  title: 'Sistem tubuh berfungsi dengan baik',
+                  items: [
+                    'Pilih makanan yang seimbang sesuai dengan kebutuhan seperti Nutrisi dan Gizinya',
+                    'Tubuh harus harus mendapatkan makanan yang seimbang seperti karbohidrat, protein, serat dan buah yang mengandung bnayaknya vitamin'
+                  ]
+                },
+                {
+                  title: 'Sistem tubuh berfungsi dengan baik',
+                  items: [
+                    'Sajikan makanan dalam bentuk yang sesuai dengan kebutuhan tubuh',
+                    'Siapkan makanan sesuai dengan kapasitas makanan yang baik dan menyehatkan'
+                  ]
+                },
+                {
+                  title: 'Sistem tubuh berfungsi dengan baik',
+                  items: [
+                    'Makanlah makanan yang tidak mengandung perasa, pewarna dan pengawet buatan',
+                    'Makanan yang mengandung gula, garam dan lemak yang berlebihan dianjurkan untuk dikurangin'
+                  ]
+                },
+              ].map((section, index) => (
+                <div key={index} className="inline-block mb-1">
+                  <div className="flex py-5 gap-2 px-5 rounded-lg shadow-lg border">
+                    <div className="w-2 rounded-md bg-sky-500 block"></div>
+                    <div>
+                      <h1 className="font-medium font-signika">{section.title}</h1>
+                      <ul className="list-disc pl-5 space-y-1">
+                        {section.items.map((item, idx) => (
+                          <li key={idx}>{item}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+  </div>
+        </div>
+
+        <div>
+          <div className="p-6">
+            <h1 className="text-center font-signika text-3xl">Dampak apabila tidak mengonsumsi makanan bergizi</h1>
+            
+            <div className="inline-block h-auto bg-red-50 border py-3 px-5 shadow-lg rounded-md">
+              <div className="flex items-center gap-2 font-signika">Penurunan sistem imun <MdAdd onClick={() => setPage(!page)} /></div>
+              <div className={`${page ? 'hidden' : 'block'}`}>
+                <p className="max-w-[500px]">Tubuh yang tidak mendapatkan asupan nutrisi yang cukup akan lebih rentan terhadap infeksi dan penyakit. 
+                  Sistem kekebalan tubuh memerlukan nutrisi yang tepat untuk berfungsi secara optimal.</p>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    </div>
-
-  </div>
-</div>
-
-
+        </div>
+        
     </div>
   );
 };
