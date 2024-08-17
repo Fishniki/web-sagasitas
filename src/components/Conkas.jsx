e37c1461754396b38782edbfcbd2489b492e0ea4
Fishniki <nikichum96@gmail.com>
2024-08-17 16:49:35 +0700

    new

--- src/components/Conkas.jsx
+++ src/components/Conkas.jsx
@@ -25,32 +25,18 @@ function Conkas() {
             </h1>
             {/* Kondisi untuk menampilkan atau menyembunyikan materi */}
             {isOpen && (
-              <div className="max-w-96 md:max-w-[500px] text-justify mb-5 font-roboto p-5 bg-sky-500 bg-opacity-15 rounded-md overflow-y-scroll">
-                Satu dari 12 anak dibawah usia
-                lima tahun di Indonesia mengalami
-                wasting dan satu dari lima mengalami
-                stunting. Terdapat perbedaan
-                angka prevalensi wasting yang besar
-                antar provinsi, dengan prevalensi
-                wasting sebesar 11,9% di Provinsi
-                Maluku dan 2.8% di Provinsi Bali.
-                Stunting dan wasting adalah akibat
-                dari gizi yang kurang optimal sejak
-                dari dalam kandungan, asupan gizi
-                yang kurang pada anak usia dini dan/
-                atau penyakit infeksi serta penyakit
-                lainnya. Kedua bentuk kekurangan
-                gizi ini memiliki dampak buruk dan
-                mengancam kesehatan, kehidupan dan
-                perkembangan jangka panjang pada
-                bayi dan anak di seluruh Indonesia. 
+              <div className="max-w-96 md:max-w-[500px] max-h-[80vh] text-start mb-5 font-roboto p-5 bg-sky-500 bg-opacity-15 rounded-md overflow-y-auto">
+                  Satu dari 12 anak dibawah usia lima tahun di Indonesia mengalami wasting dan satu dari lima mengalami stunting.
+                  Terdapat perbedaan angka prevalensi wasting yang besar antar provinsi, dengan prevalensi wasting sebesar 11,9% di Provinsi Maluku dan 2.8% di Provinsi Bali.
+                  Stunting dan wasting adalah akibat dari gizi yang kurang optimal sejak dari dalam kandungan, asupan gizi yang kurang pada anak usia dini dan/atau penyakit infeksi serta penyakit lainnya.
+                  Kedua bentuk kekurangan gizi ini memiliki dampak buruk dan mengancam kesehatan, kehidupan dan perkembangan jangka panjang pada bayi dan anak di seluruh Indonesia.
               </div>
             )}
             <button
               onClick={toggleMaterial}
               className="inline px-5 py-2 rounded-md font-anton bg-orange-400 text-white"
             >
-              {isOpen ? 'Close' : 'Open'}
+              {isOpen ? 'Tutup' : 'Selengkapnya'}
             </button>
           </div>
         </div>
